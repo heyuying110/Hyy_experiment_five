df19c3030d01f12f818f9d6c204fdcdcd308d6c3
heyuying110 <704872770@qq.com>
2018-04-28 01:09:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/hyy_experiment_five/ScreenPreference.java
@@ -0,0 +1,13 @@
+package com.example.administrator.hyy_experiment_five;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenPreference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_preference);
+    }
+}
